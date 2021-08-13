de366054232e66b7172c507240b655b8eb6c1e65
DINESH KUMAR MAMIDELA <47123679+dinesh0002@users.noreply.github.com>
2021-08-13 10:38:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"221810402010@gitam.in\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
-port = $PORT\n\
 enableCORS = false\n\
+port = $PORT\n\
 \n\
 " > ~/.streamlit/config.toml
